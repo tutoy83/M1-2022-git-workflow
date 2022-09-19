96a3f84719bcfe37b35b00c54adc5cd9aac42824
Rémi GRISOT <remi.grisot@isen.yncrea.fr>
2022-09-19 15:05:25 +0200

    Improves speed of conversion
    Uses bitshift that are faster to compute than powers / divisions

--- converter.py
+++ converter.py
@@ -3,10 +3,10 @@ from typing import List
 
 def dec_to_bin(n: int) -> List[int]:
     bit = [0 if n % 2 == 0 else 1]
-    return bit if n <= 1 else dec_to_bin(n // 2) + bit
+    return bit if n <= 1 else dec_to_bin(n >> 2) + bit
 
 
 def bin_to_dec(bits_array: List[int]) -> int:
     ba = bits_array.copy()
     ba.reverse()
-    return sum([2 ** i * m for i, m in enumerate(ba)])
+    return sum([1 << i * m for i, m in enumerate(ba)])
