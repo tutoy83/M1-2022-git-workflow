cd3b00042872382ee620aea6733891cdfe6c7e01
Rémi GRISOT <remi.grisot@isen.yncrea.fr>
2022-09-19 14:50:13 +0200

    Adds a binary to decimal converter
    Function that converts a array of 0s and 1s to an int

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from converter import dec_to_bin
+from converter import dec_to_bin, bin_to_dec
 
 
 def say_hello():
@@ -7,3 +7,4 @@ def say_hello():
 
 if __name__ == '__main__':
     print(*dec_to_bin(256))
+    print(bin_to_dec([1, 0, 1, 0, 0, 1]))
